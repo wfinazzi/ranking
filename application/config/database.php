7920e7eed6d1ba38e604356bfc33d694000545c5
Wellington Almeida <wellingtonfinazzi@gmail.com>
2020-07-21 18:18:32 -0300

    Escondendo base

--- application/config/database.php
+++ application/config/database.php
@@ -73,29 +73,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-/*
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => '23.229.222.66',
-	'username' => 'wfinazzi',
-	'password' => 'Grotkowsk1',
-	'database' => 'consegguarulhos',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-*/
 
 $db['default'] = array(
 	'dsn'	=> '',
